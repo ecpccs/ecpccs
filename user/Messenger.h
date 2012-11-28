4d8f57aecd74b91499d8e37eda823141763f8d09
Averty Benoit <benoit.averty@gmail.com>
2012-11-28 20:01:00 +0100

    changed port number

--- user/Messenger.h
+++ user/Messenger.h
@@ -13,7 +13,7 @@ class Messenger
     public:
         Messenger(std::string login, std::string authorityIp);
        
-        void listen(unsigned int port = 65535);
+        void listen(unsigned int port = 65534);
 
         void retrieveRemoteUser(std::string login);
         void sendTo(std::string login, std::string message);
